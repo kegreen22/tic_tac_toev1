c367e456911a1834fe6ba1b47d32dc96444b8f94
Keith Green <kegreen22@gmail.com>
2015-10-07 23:43:28 -0400

    10-7-15 improved board readability; simplified while loop

--- tic-tac-toe_v1.rb
+++ tic-tac-toe_v1.rb
@@ -8,21 +8,28 @@ class Match
   attr_accessor :board, :game_over, :winner
 
   def turn(player_name, player_icon)
-    puts "Please make your move #{player_name} -- remember to choose a number between 0 and 8 (or q to quit)"
-    action = gets.chomp
-    if action == "q"   # end the game via quitting
-    @winner = "noone"
-    @game_over = true
-    # the game is over without a winner
-    end
+    if @board.include?("E")
 
-    if valid_move(action)   # check the validity of the player's move
-    @board[action.to_i] = player_icon
-    victory_check(player_name, player_icon)   # check victory conditions at end of each player's turn
+      puts "Please make your move #{player_name} -- remember to choose a number between 0 and 8 (or q to quit)"
+      action = gets.chomp
+
+      if action.to_s == "q"   # end the game via quitting
+      @winner = "noone"
+      @game_over = true
+      # the game is over without a winner
+     end
+
+      if valid_move(action)   # check the validity of the player's move
+      @board[action.to_i] = player_icon
+     victory_check(player_name, player_icon)   # check victory conditions at end of each player's turn
+     else
+      turn(player_name, player_icon)
+      end   # end of if statement
+
     else
-    turn(player_name, player_icon)
-    end   # end of if statement
-
+      @winner = "noone"
+      @game_over = true
+    end # end of if@board statement
 
     # return # return to execute the game's main while loop
 
@@ -31,20 +38,20 @@ class Match
 
   def valid_move(action)
 
-    if ((action.to_i > 8) || (action.to_i < 0))   # chosen number must be valid (0-8)
-    puts "The number chosen must be between 0 and 8"
-    return false
+      if ((action.to_i > 8) || (action.to_i < 0))   # chosen number must be valid (0-8)
+      puts "The number chosen must be between 0 and 8"
+      return false
 
-    elsif
-    @board[action.to_i] == "" # if the square is empty return true (it is a valid choice)
-    return true
+      elsif
+      @board[action.to_i] == "E" # if the square is empty return true (it is a valid choice)
+      return true
 
-    else
-    puts "The square chosen is not empty. Please make another choice."
-    show_board
-    return false
+      else
+      puts "The square chosen is not empty. Please make another choice."
+      show_board
+      return false
 
-    end  # end of if statement
+      end  # end of if statement
 
   end # end of valid_move method
 
@@ -61,6 +68,10 @@ class Match
     victory_6 = [@board[2], @board[5], @board[8]]
     victory_7 = [@board[0], @board[4], @board[8]]
     victory_8 = [@board[2], @board[4], @board[6]]
+
+    puts victory_1
+    puts victory_2
+    puts victory_8
 
     if ((player_victory.eql?(victory_1)) #|| (player_victory.eql?(victory_2)) || (player_victory.eql?(victory_3)) || (player_victory.eql?(victory_4)) ||
 (player_victory.eql?(victory_5)) || (player_victory.eql?(victory_6)) || (player_victory.eql?(victory_7)) || (player_victory.eql?(victory_8)))
@@ -91,9 +102,12 @@ a row based on the game board
 
 
   def show_board   # show the game board
-    puts @board[0..2]
-    puts @board[3..5]
-    puts @board[6..8]
+    print @board[0..2]
+    puts "\n"
+    print @board[3..5]
+    puts "\n"
+    print @board[6..8]
+    puts "\n"
   end   # end of method
 
 
@@ -110,7 +124,7 @@ player2 = Player.new
 player1.icon = "X"
 player2.icon = "O"
 tic_tac = Match.new   # start new tic tac toe game
-tic_tac.board = Array.new(9, "")   # create board with 9 empty squares set to ""
+tic_tac.board = Array.new(9, "E")   # create board with 9 empty squares set to "E"
 tic_tac.game_over = false
 tic_tac.winner = "noone"   # noone wins the game until a winner is declared
 game_turn = 1   # start from turn 1
@@ -122,13 +136,14 @@ puts "Please enter the second player's name"
 player2.name = gets.chomp
 
 
-while (!tic_tac.game_over || tic_tac.board.include?("")) # game is over when game is over or when all squares are taken without a winner (game over)
+while (tic_tac.game_over == false) # game is over when game is over or when all squares are taken without a winner (game over)
   show_game_turn(game_turn)
   tic_tac.show_board
   tic_tac.turn(player1.name, player1.icon)
   tic_tac.show_board
   tic_tac.turn(player2.name, player2.icon)
   game_turn+=1
+
 
 end  # end of the while statement
 
