ea68d91e31a9753c1c295c9a1e15a1c7afe41893
Keith Green <kegreen22@gmail.com>
2016-02-08 19:00:16 -0500

    first commit of shell

---
+++ binary_tree.rb
@@ -0,0 +1,20 @@
+# binary tree
+
+class Node
+  attr_accessor :parent_node, :left_node, :right_node
+
+  def initialize(value)
+    @value = value
+
+  end
+
+  def search  # search insie the binary tree
+
+  end
+
+
+  def build_tree(data)  # create the binary tree
+
+  end
+
+end
